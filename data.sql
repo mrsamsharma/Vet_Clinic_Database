5b72a6208f414619c2f22b72e582d455766928db
Satyam Sharma <samwfelice@gmail.com>
2023-01-25 07:34:13 -0800

    Fix Syntax for schema and data

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES ('Agumon', '02-03-2020', 0, True, 10.23);
-INSERT INTO animals VALUES ('Gabumon', '11-15-2019', 2, True, 8);
-INSERT INTO animals VALUES ('Pikachu', '01-07-2021', 1, False, 15.04);
-INSERT INTO animals VALUES ('Devimon', '05-12-2017', 5, True, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, True, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '11-15-2019', 2, True, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '01-07-2021', 1, False, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '05-12-2017', 5, True, 11);

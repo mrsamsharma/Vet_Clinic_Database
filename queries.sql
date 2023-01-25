425f24637ca4593c1c9fccbad00b4da0099579d3
Satyam Sharma <samwfelice@gmail.com>
2023-01-25 07:54:42 -0800

    Create queries for the data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon%';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-12';
+SELECT * from animals WHERE neutered = 't' AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 OR weight_kg = 10.4 OR weight_kg = 17.3;

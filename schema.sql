f12c8cc20f2f011e65db0ca589eb0d70af5af44c
Satyam Sharma <samwfelice@gmail.com>
2023-01-28 05:24:15 -0800

    Update Data in Animals table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,8 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY, 
     name varchar(100)
 );
+
+ALTER TABLE animals ADD COLUMN species_ID INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_ID) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_ID INT;
+ALTER TABLE animals ADD FOREIGN KEY (owners_ID) REFERENCES owners(id);
